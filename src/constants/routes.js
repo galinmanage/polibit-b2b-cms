bfb04c7ddeb854bde469dd2de3685e324d1fd69e
Michael <michaelb@inmanage.net>
2025-12-23 17:24:03 +0200

    Add Direct Quoting routes and subroutes to CMS

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,11 @@
 const ROOT = '/';
 const LOGIN = '/login';
-const USERS = `/users`;
-const TEXTS = `/translations`;
-const MEDIA = `/media`;
+const DIRECT_QUOTING = '/direct-quoting';
+const ELIGIBLE_MODELS = '/direct-quoting/eligible-models';
+const API_ROUTED_COMPANIES = '/direct-quoting/api-routed-companies';
+const BASE_PREMIUMS = '/direct-quoting/base-premiums';
+const PREMIUM_EXCEPTIONS = '/direct-quoting/premium-exceptions';
+const PRICING_FACTORS = '/direct-quoting/pricing-factors';
 
 // The permissions are from the gd
 const ROUTES = {
@@ -23,30 +26,52 @@ const ROUTES = {
     showOnMenu: false,
     index: 'Dashboard',
     subs: {
-      TextsPage: {
-        text: 'cms_navItem_texts',
-        path: TEXTS,
-        component: 'TextsPage',
-        isProtected: true,
-        showOnMenu: true,
-        subs: {},
-      },
-      MediaPage: {
-        text: 'cms_navItem_media',
-        path: MEDIA,
-        component: 'MediaPage',
-        isProtected: true,
-        showOnMenu: true,
-        subs: {},
-      },
-      // 'UsersPage': {
-      //     text: 'cms_navItem_users',
-      //     path: USERS,
-      //     component: 'UsersPage',
-      //     isProtected: true,
-      //     showOnMenu: true,
-      //     subs: {}
+      // DirectQuoting: {
+      //   text: 'cms_navItem_directQuoting',
+      //   isProtected: true,
+      //   showOnMenu: true,
+      //   subs: {},
       // },
+      EligibleModels: {
+        text: 'cms_navItem_eligibleModels',
+        path: ELIGIBLE_MODELS,
+        component: 'EligibleModels',
+        isProtected: true,
+        showOnMenu: true,
+        subs: {},
+      },
+      ApiRoutedCompanies: {
+        text: 'cms_navItem_apiRoutedCompanies',
+        path: API_ROUTED_COMPANIES,
+        component: 'ApiRoutedCompanies',
+        isProtected: true,
+        showOnMenu: true,
+        subs: {},
+      },
+      BasePremiums: {
+        text: 'cms_navItem_basePremiums',
+        path: BASE_PREMIUMS,
+        component: 'BasePremiums',
+        isProtected: true,
+        showOnMenu: true,
+        subs: {},
+      },
+      PremiumExceptions: {
+        text: 'cms_navItem_premiumExceptions',
+        path: PREMIUM_EXCEPTIONS,
+        component: 'PremiumExceptions',
+        isProtected: true,
+        showOnMenu: true,
+        subs: {},
+      },
+      PricingFactors: {
+        text: 'cms_navItem_pricingFactors',
+        path: PRICING_FACTORS,
+        component: 'PricingFactors',
+        isProtected: true,
+        showOnMenu: true,
+        subs: {},
+      },
     },
   },
   404: {
