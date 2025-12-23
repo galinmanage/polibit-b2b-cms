88f4006072f73a4d142cf303834ee658c64c7be9
Michael <michaelb@inmanage.net>
2025-12-23 17:25:05 +0200

    Replace CMS components with Direct Quoting components in index export

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,8 @@
 export { default as Home } from './home';
 export { default as Page404 } from './page404';
-
 export { default as Login } from './login';
-
-export { default as UsersPage } from './users';
-export { default as TextsPage } from './texts';
-export { default as MediaPage } from './media';
+export { default as EligibleModels } from './direct-quoting/eligible-models';
+export { default as ApiRoutedCompanies } from './direct-quoting/api-routed-companies';
+export { default as BasePremiums } from './direct-quoting/base-premiums';
+export { default as PremiumExceptions } from './direct-quoting/premium-exceptions';
+export { default as PricingFactors } from './direct-quoting/pricing-factors';
